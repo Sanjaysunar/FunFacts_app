c8e623bf3985fa8ceb3d5dd493a7369ee6ee716f
Sanjay Sunar <sansunar11@gmail.com>
2022-07-23 17:04:10 +0530

    Update FunFactsActivity.java
    - Added facts array with facts on it

--- src/main/java/com/example/funfacts/FunFactsActivity.java
+++ src/main/java/com/example/funfacts/FunFactsActivity.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.util.Locale;
 import java.util.Random;
 
 public class FunFactsActivity extends AppCompatActivity {
@@ -25,11 +26,25 @@ public class FunFactsActivity extends AppCompatActivity {
         View.OnClickListener listener = new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                String[] facts = {
+                        "Ants stretch when they wake up in the morning.",
+                        "Ostriches can run faster than horse.",
+                        "Olympic gold medals are actually made mostly of silver.",
+                        "You can born with 300 bones; by the time you are an adult you will have 206.",
+                        "It takes about 8 minutes for light from the Sun to reach Earth.",
+                        "Some bamboo plants can grow almost a meter in just one day.",
+                        "The state of Florida is bigger than England.",
+                        "Some penguins can leap 2-3 meters out of the water.",
+                        "On average, it takes 66 days to form a new habit.",
+                        "Mammoths still walked the earth when the Great Pyramid was being built."
+                };
+
+
                 // The button was clicked, so update the fact TextView with a new fact
                 // Randomly select a fact
                 Random randomGenerator = new Random();
-                int randomNumber = randomGenerator.nextInt(3);
-                String fact = randomNumber + "";
+                int randomNumber = randomGenerator.nextInt(facts.length);
+                String fact = facts[randomNumber];
 
                 //Update the screen with our new fact
                 factTextView.setText(fact);
