326c9c8930129f855cf0ec03a87626c044d1d99f
Sanjay Sunar <sansunar11@gmail.com>
2022-07-23 16:26:45 +0530

    Update FunFactsActivity.java
    - Declare out view variable
    - Assign View form the layout file to the corresponding variable
    - Add Button clicked function

--- src/main/java/com/example/funfacts/FunFactsActivity.java
+++ src/main/java/com/example/funfacts/FunFactsActivity.java
@@ -2,12 +2,34 @@ package com.example.funfacts;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class FunFactsActivity extends AppCompatActivity {
+    // Declare out view variable
+    private TextView factTextView;
+    private Button showFactButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Assign the Views form the layout file to the corresponding variable
+        factTextView = findViewById(R.id.factTextView);
+        showFactButton = findViewById(R.id.showFactButton);
+
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // The button was clicked, so update the fact TextView with a new fact
+                String fact = "Ostriches can run faster than horses";
+                factTextView.setText(fact);
+            }
+        };
+        showFactButton.setOnClickListener(listener);
+
+
     }
 }
