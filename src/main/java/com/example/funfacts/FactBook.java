1c75343380c52488c16206875e1e99b0fb513384
Sanjay Sunar <sansunar11@gmail.com>
2022-07-25 23:21:04 +0530

    Update in FactBook, activity xml & FunFactsActivity
    - Make facts privately accessible for factbook
    -import factbook in funfactactivity
    -added color changing to red feature

--- src/main/java/com/example/funfacts/FactBook.java
+++ src/main/java/com/example/funfacts/FactBook.java
@@ -4,24 +4,22 @@ import java.util.Random;
 
 public class FactBook {
     // Fields or Member Variables  -  Properties about the object
+    private String[] facts = {
+            "Ants stretch when they wake up in the morning.",
+            "Ostriches can run faster than horse.",
+            "Olympic gold medals are actually made mostly of silver.",
+            "You can born with 300 bones; by the time you are an adult you will have 206.",
+            "It takes about 8 minutes for light from the Sun to reach Earth.",
+            "Some bamboo plants can grow almost a meter in just one day.",
+            "The state of Florida is bigger than England.",
+            "Some penguins can leap 2-3 meters out of the water.",
+            "On average, it takes 66 days to form a new habit.",
+            "Mammoths still walked the earth when the Great Pyramid was being built."
+    };
 
     // Methods  -  Actions the object can take
-    public String getFact() {
-        String[] facts = {
-                "Ants stretch when they wake up in the morning.",
-                "Ostriches can run faster than horse.",
-                "Olympic gold medals are actually made mostly of silver.",
-                "You can born with 300 bones; by the time you are an adult you will have 206.",
-                "It takes about 8 minutes for light from the Sun to reach Earth.",
-                "Some bamboo plants can grow almost a meter in just one day.",
-                "The state of Florida is bigger than England.",
-                "Some penguins can leap 2-3 meters out of the water.",
-                "On average, it takes 66 days to form a new habit.",
-                "Mammoths still walked the earth when the Great Pyramid was being built."
-        };
+    String getFact() {
 
-
-        // The button was clicked, so update the fact TextView with a new fact
         // Randomly select a fact
         Random randomGenerator = new Random();
         int randomNumber = randomGenerator.nextInt(facts.length);
--- src/main/java/com/example/funfacts/FunFactsActivity.java
+++ src/main/java/com/example/funfacts/FunFactsActivity.java
@@ -1,18 +1,22 @@
 package com.example.funfacts;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import java.util.Locale;
 import java.util.Random;
 
 public class FunFactsActivity extends AppCompatActivity {
+    private FactBook factBook = new FactBook();
     // Declare out view variable
     private TextView factTextView;
     private Button showFactButton;
+    private RelativeLayout relativeLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,13 +26,16 @@ public class FunFactsActivity extends AppCompatActivity {
         // Assign the Views form the layout file to the corresponding variable
         factTextView = findViewById(R.id.factTextView);
         showFactButton = findViewById(R.id.showFactButton);
+        relativeLayout = findViewById(R.id.relativeLayout);
 
         View.OnClickListener listener = new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                String fact = factBook.getFact();
 
                 //Update the screen with our new fact
                 factTextView.setText(fact);
+                relativeLayout.setBackgroundColor(Color.RED);
             }
         };
         showFactButton.setOnClickListener(listener);
