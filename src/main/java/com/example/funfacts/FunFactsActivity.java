39239ef063c55dad639e20bfa53ef57172c89b2f
Sanjay Sunar <sansunar11@gmail.com>
2022-07-26 20:25:09 +0530

    Update FunFactsActivity.java

--- src/main/java/com/example/funfacts/FunFactsActivity.java
+++ src/main/java/com/example/funfacts/FunFactsActivity.java
@@ -3,6 +3,7 @@ package com.example.funfacts;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RelativeLayout;
@@ -13,6 +14,7 @@ import java.util.Locale;
 import java.util.Random;
 
 public class FunFactsActivity extends AppCompatActivity {
+    public static final String TAG = FunFactsActivity.class.getSimpleName();
     private FactBook factBook = new FactBook();
     private ColorWheel colorWheel = new ColorWheel();
     // Declare out view variable
@@ -30,29 +32,30 @@ public class FunFactsActivity extends AppCompatActivity {
         //if it is not Null then call hide function
         if (getSupportActionBar() != null) {
             getSupportActionBar().hide();
+        }
 
 
-            // Assign the Views form the layout file to the corresponding variable
-            factTextView = findViewById(R.id.factTextView);
-            showFactButton = findViewById(R.id.showFactButton);
-            relativeLayout = findViewById(R.id.relativeLayout);
+        // Assign the Views form the layout file to the corresponding variable
+        factTextView = findViewById(R.id.factTextView);
+        showFactButton = findViewById(R.id.showFactButton);
+        relativeLayout = findViewById(R.id.relativeLayout);
 
-            View.OnClickListener listener = new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    String fact = factBook.getFact();
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String fact = factBook.getFact();
 
-                    //Update the screen with our new fact
-                    factTextView.setText(fact);
+                //Update the screen with our new fact
+                factTextView.setText(fact);
 
-                    int color = colorWheel.getColor();
-                    relativeLayout.setBackgroundColor(color);
-                    showFactButton.setTextColor(color);
-                }
-            };
-            showFactButton.setOnClickListener(listener);
+                int color = colorWheel.getColor();
+                relativeLayout.setBackgroundColor(color);
+                showFactButton.setTextColor(color);
+            }
+        };
+        showFactButton.setOnClickListener(listener);
 
 //        Toast.makeText(this, "Yay! Our Activity was created!", Toast.LENGTH_SHORT).show();
-        }
+        Log.d(TAG, "We're logging from the onCreate() method!");
     }
 }
